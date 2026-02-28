8f2a4148c49aff6931c4380bf3258f7a8bd16fb1
rainbow686 <rainbow686@github.com>
2026-02-28 18:32:03 +0800

    fix: convert API handler to CommonJS (module.exports)

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,4 @@
-export default async function handler(req, res) {
-  // CORS
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -38,4 +37,4 @@ export default async function handler(req, res) {
     console.error(err);
     return res.status(500).json({ error: 'Internal server error' });
   }
-}
+};
